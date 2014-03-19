f11c73c354353bf374f17c861c7b620385b6788a
Dennis Günnewig <dg1@vrnetze.de>
2014-03-19 09:04:32 +0100

    deleted unneccassary code

--- lib/proxy_pac_rb/environment.rb
+++ lib/proxy_pac_rb/environment.rb
@@ -28,12 +28,8 @@ module ProxyPacRb
         :isResolvable,
         :isInNet,
         :dnsResolve,
-#        :MyIpAddress ,
         :dnsDomainLevels,
         :shExpMatch,
-#        :weekdayRange,
-#        :dateRange,
-#        :timeRange,
       ]
     end
 
@@ -65,10 +61,6 @@ module ProxyPacRb
       resolve_host(host)
     end
 
-    def myIpAddress 
-      IPAddr.new(client_ip).to_s
-    end
-
     def dnsDomainLevels(host)
       host.scan(".").size
     end
@@ -77,89 +69,7 @@ module ProxyPacRb
       ::File.fnmatch(shexp, str)
     end
 
-    def weekdayRange(wd1, wd2 = nil, gmt = nil)
-      fail Exceptions::InvalidArgument, "wd1 needs to be one of #{days.keys.collect {|k| "\"#{k}\""}.join(', ')}" unless days.key?(wd1)
-      fail Exceptions::InvalidArgument, "wd2 needs to be one of #{days.keys.collect {|k| "\"#{k}\""}.join(', ')}" if wd2 and !days.key?(wd2)
-
-      if gmt == "GMT"
-        local_time = time.utc 
-      else
-        local_time = time
-      end
-
-      (days[wd1]..days[wd2 || wd1]).include? local_time.wday
-    end
-
-    def dateRange(*args)
-      fail Exceptions::InvalidArgument, "range needs to be one of #{months.keys.collect {|k| "\"#{k}\""}.join(', ')}" if args.any? { |a| !months.key?(a)  }
-
-      if args.last == "GMT" and args.pop
-        local_time = time.utc 
-      else
-        local_time = time
-      end
-
-      case args.size
-      when 1
-        check_date_part(local_time, args[0])
-      when 2
-        check_date_part(local_time, args[0]..args[1])
-      when 4
-        check_date_part(local_time, args[0]..args[2]) and
-          check_date_part(local_time, args[1]..args[3])
-      when 6
-        check_date_part(local_time, args[0]..args[3]) and
-          check_date_part(local_time, args[1]..args[4]) and
-          check_date_part(local_time, args[2]..args[5])
-      else
-        fail ArgumentError, "wrong number of arguments"
-      end
-    end
-
-    def timeRange(*args)
-      fail Exceptions::InvalidArgument, "args need to be integer values" if args.any? { |a| !a.kind_of? Fixnum }
-
-      if args.last == "GMT" and args.pop
-        local_time = time.utc 
-      else
-        local_time = time
-      end
-
-      case args.size
-      when 1
-        local_time.hour == args[0]
-      when 2
-        (args[0]..args[1]).include? local_time.hour
-      when 4
-        (args[0]..args[2]).include? local_time.hour and
-          (args[1]..args[3]).include? local_time.min
-      when 6
-        (args[0]..args[3]).include? local_time.hour and
-          (args[1]..args[4]).include? local_time.min  and
-          (args[2]..args[5]).include? local_time.sec
-      else
-        fail ArgumentError, "wrong number of arguments"
-      end
-    end
-
     private
-
-    def check_date_part(time, part, operation = :==)
-      case part
-      when String
-        time.month.send(operation, months[part])
-      when Integer
-        if part < 100
-          time.day.send(operation, part)
-        else
-          time.year.send(operation, part)
-        end
-      when Range
-        check_date_part(time, part.begin, :>=) and check_date_part(time, part.end, :<=)
-      else
-        fail ArgumentError, "wrong type"
-      end
-    end
 
     def resolve_host(host)
       Resolv.each_address(host) do |address|
