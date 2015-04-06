f54d3f80ad377fafe313876d29c387ea6e0c71b0
Max Meyer <dev@fedux.org>
2015-04-06 11:37:48 +0200

    Migrate code to pac file

--- lib/proxy_pac_rb/proxy_pac_file.rb
+++ lib/proxy_pac_rb/proxy_pac_file.rb
@@ -24,7 +24,24 @@ module ProxyPacRb
     end
 
     def valid?
-      valid == true
+      valid == true && readable?
+    end
+
+    def javascript?
+      !javascript.nil?
+    end
+
+    def find(url, fail_safe: true)
+      if fail_safe == true
+        fail ProxyPacInvalidError, "The proxy.pac \"#{source}\" is not readable. Stopping here." unless readable?
+        fail ProxyPacInvalidError, "The proxy.pac \"#{source}\" is not valid: #{message}. Stopping here." unless valid?
+        fail ProxyPacInvalidError, "The proxy.pac \"#{source}\" is could not be parsed: #{message}. Stopping here." unless javascript?
+      end
+
+      uri = Addressable::URI.heuristic_parse(url)
+      fail UrlInvalidError, 'url is missing host' unless uri.host
+
+      javascript.FindProxyForURL(uri.to_s, uri.host)
     end
   end
 end
