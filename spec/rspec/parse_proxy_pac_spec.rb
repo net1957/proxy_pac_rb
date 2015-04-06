47728b16384231bf69db11763c5251e0a342ab37
Max Meyer <dev@fedux.org>
2015-04-06 20:23:42 +0200

    Add another example

--- spec/rspec/parse_proxy_pac_spec.rb
+++ spec/rspec/parse_proxy_pac_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe 'Parse proxy.pac', type: :proxy_pac do
   subject do
     <<-EOS.strip_heredoc.chomp
       function FindProxyForURL(url, host) {
-        return "DIRECT";
+        if (dnsDomainIs(host, 'localhost') {
+          return "DIRECT";
+        } else {
+          return "PROXY proxy.example.com:3128";
+        }
       }
     EOS
   end
@@ -14,6 +18,11 @@ RSpec.describe 'Parse proxy.pac', type: :proxy_pac do
     context 'http://localhost' do
       let(:url) { 'http://localhost' }
       it { expect(url).to be_downloaded_via 'DIRECT' }
+    end
+
+    context 'http://www.example.org' do
+      let(:url) { 'http://www.example.org' }
+      it { expect(url).not_to be_downloaded_via 'DIRECT' }
     end
   end
 end
