2755a0d78aef12bdf86278c08297adde5014fc55
Dennis Günnewig <dg1@ratiodata.de>
2015-05-06 13:39:52 +0200

    Truncate longer proxy.pac files to make the output easier to read

--- lib/proxy_pac_rb.rb
+++ lib/proxy_pac_rb.rb
@@ -1,5 +1,6 @@
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/string/strip'
+require 'active_support/core_ext/string/filters'
 require 'addressable/uri'
 require 'ipaddr'
 require 'thor'
