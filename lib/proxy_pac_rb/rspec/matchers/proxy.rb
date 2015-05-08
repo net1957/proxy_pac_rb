c44fa74d2582283d94aa50243865ac658f1663e7
Dennis Günnewig <dg1@ratiodata.de>
2015-05-08 11:49:54 +0200

    Use method which is available in active support 4.1

--- lib/proxy_pac_rb/rspec/matchers/proxy.rb
+++ lib/proxy_pac_rb/rspec/matchers/proxy.rb
@@ -18,10 +18,10 @@ RSpec::Matchers.define :be_the_same_proxy_pac_file do |expected|
   diffable
 
   failure_message do
-    format(%(expected that proxy.pac "%s" is equal to proxy.pac "%s", but it is not equal.), @file_a.source.truncate_words(10), @file_b.source.truncate_words(10))
+    format(%(expected that proxy.pac "%s" is equal to proxy.pac "%s", but it is not equal.), @file_a.source.truncate(30), @file_b.source.truncate(30))
   end
 
   failure_message_when_negated do
-    format(%(expected that proxy.pac "%s" is not equal to proxy.pac "%s", but it is equal.), @file_a.source.truncate_words(10), @file_b.source.truncate_words(10))
+    format(%(expected that proxy.pac "%s" is not equal to proxy.pac "%s", but it is equal.), @file_a.source.truncate(30), @file_b.source.truncate(30))
   end
 end
