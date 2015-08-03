170d6ce39d910fd76d469094aed5caff8a32f23e
Max Meyer <dev@fedux.org>
2015-08-03 06:34:04 +0200

    Use new aruba configuration

--- spec/support/aruba.rb
+++ spec/support/aruba.rb
@@ -1,23 +1,6 @@
 # encoding: utf-8
-require 'aruba/api'
-require 'aruba/reporting'
+require 'aruba/rspec'
 
-# Spec Helpers
-module SpecHelper
-  # Helpers for aruba
-  module Aruba
-    include ::Aruba::Api
-
-    def dirs
-      @dirs ||= %w(tmp rspec)
-    end
-  end
-end
-
-RSpec.configure do |c|
-  c.include SpecHelper::Aruba
-  c.before :each do
-    clean_current_directory
-    restore_env
-  end
+Aruba.configure do |config|
+  config.working_directory = 'tmp/rspec'
 end
